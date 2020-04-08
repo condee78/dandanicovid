1b7395a8cd34017b91dc34b234e7697570c34d8b
lottechem\61205021 <fazri.aziz@lottechem.co.id>
2020-04-08 14:42:08 +0700

    update website title, footer and information

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { Col, Row } from "reactstrap";
 
 const propTypes = {
   children: PropTypes.node,
@@ -9,14 +11,26 @@ const defaultProps = {};
 
 class DefaultFooter extends Component {
   render() {
-
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2019 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <Row>
+          <span>
+            <p className="text-center mb-0">
+              <a href="#">DandaniCovid19</a> &copy; 2020 Made with ❤ by
+              Fazriaziz. Powered by{" "}
+              <a href="https://coreui.io/react">CoreUI for React</a>
+            </p>
+            <p>
+              <em>
+                Data berasal dari worldometers, WHO, J John Hopkins University
+                CSSE, dan Organisasi terkemuka di Dunia.
+              </em>
+            </p>
+          </span>
+        </Row>
       </React.Fragment>
     );
   }
